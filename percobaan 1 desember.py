79f7af7ba0e2ac88b6d513e0795c3425f6648905
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-12-01 13:38:04 +0800

    Add files via upload

---
+++ percobaan 1 desember.py
@@ -0,0 +1,13 @@
+print(f"{'PROGRAM UNTUK  MENGHITUNG LUAS SEGI TIGA':^50}")
+print(f"{'='*50 : ^50}")
+
+alas = float(input("Masukkan Alas Segi Tiga : "))
+tinggi = float(input("Masukkan Tinggi Segi Tiga : "))
+
+luas = alas * tinggi /2
+
+print(f"{'='*50:^50}")
+print(f"{'LUAS SEGI TIGA':^50}")
+print(f"{'='*50:^50}")
+
+print(f"Luas Segi Tiga Adalah {luas}")
